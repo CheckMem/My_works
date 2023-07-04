3826959598774830b8bb3cafe10df11b05b85ba2
man <nikitka_gerasin@mail.ru>
2023-07-04 15:10:33 +0300

    JSON Dumbs

--- 04_07 2 after dinner.py
+++ 04_07 2 after dinner.py
@@ -1,13 +1,43 @@
 import json #Java Script Object Notation
 
-weekdays = ['Понедельник', 'Вторник','Среда', 'Четверг',
-            'Пятница', 'Суббота', 'Воскресенье']
-
-st = [i for i in range(1, 6)]
 
 
-week_dict = {key: val for key, val in enumerate(weekdays)}
-print(week_dict)
+pets = {
+    'name': 'Charly',
+    'age': 15,
+    'meals': ['Purina', 'Hills'],
+    'owner': {'fname': 'John', 'sname': 'Gates'}
+}
 
-data = json.dumps(week_dict)
-print(data)
+with open('pets.json', 'w') as pet_file:
+    json.dump(pets, pet_file)
+
+# print(pet_file)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# weekdays = ['Понедельник', 'Вторник','Среда', 'Четверг',
+#             'Пятница', 'Суббота', 'Воскресенье']
+#
+# st = [i for i in range(1, 6)]
+#
+#
+# week_dict = {key: val for key, val in enumerate(weekdays)}
+# print(week_dict)
+#
+# data = json.dumps(week_dict)
+# print(data)
