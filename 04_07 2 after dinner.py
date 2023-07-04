ee0e2a2647d330e21663e4daf2e174953bd7b88e
man <nikitka_gerasin@mail.ru>
2023-07-04 14:51:52 +0300

    JSON Dumbs

--- 04_07 2 after dinner.py
+++ 04_07 2 after dinner.py
@@ -1,1 +1,13 @@
-import json
+import json #Java Script Object Notation
+
+weekdays = ['Понедельник', 'Вторник','Среда', 'Четверг',
+            'Пятница', 'Суббота', 'Воскресенье']
+
+st = [i for i in range(1, 6)]
+
+
+week_dict = {key: val for key, val in enumerate(weekdays)}
+print(week_dict)
+
+data = json.dumps(week_dict)
+print(data)
