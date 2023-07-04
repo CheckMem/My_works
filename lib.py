58b7b57b3d1f8ea3ea8fb97553449594eb35be3a
Your Name <nikitka_gerasin@mail.ru>
2023-07-04 10:55:16 +0300

    КомментарийÐ

---
+++ lib.py
@@ -0,0 +1,15 @@
+def say_hello(name):
+    print('Hello,', name)
+
+
+
+
+def div(a, b):
+    if b != 0:
+        print(f'{a / b:.1f}')
+    else:
+        print( 'Ошибка, деление на ноль')
+
+
+if __name__ == '__main__':
+     print('Это не исполняемый файл')
